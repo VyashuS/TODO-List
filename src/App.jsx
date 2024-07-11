330622faf1e72239d79ce4105e090165de618621
VyashuS <97904270+VyashuS@users.noreply.github.com>
2024-07-11 16:22:40 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <div className=" min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-4xl font-bold text-center mb-8 mt-2">
-            Keep Your TODOS Track
+            Keep Track of Your TODOS
           </h1>
           <div className="mb-6">
             <TodoForm />
